2714c31ef3e1e944ea2b268753f9db2132d78f38
yeyulin <yeyulin@zlg.cn>
2018-08-11 17:42:13 +0800

    add function login to index

--- src/js/pages/demo/hello/hello.vue
+++ src/js/pages/demo/hello/hello.vue
@@ -1,14 +1,19 @@
 <template>
     <div style="margin-top: 50px;">
         <text class="title">Hello, world.</text>
-        <wxc-button class="btn-250" text="login"></wxc-button>
+        <wxc-button class="btn-250" text="login" @wxcButtonClicked="showEros"></wxc-button>
     </div>
 </template>
 
 <script>
 import { WxcButton } from 'weex-ui';
 export default {
-    components: { WxcButton }
+    components: { WxcButton },
+    methods: {
+        showEros() {
+            this.$router.setHomePage('tabBar')
+        }
+    }
 }
 </script>
 
